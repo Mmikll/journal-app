f3d70fe6aee5f01b4b55c56dd95c4dda7ee5f204
Miguel <mmirandasanchez16@gmail.com>
2024-10-14 23:54:14 -0300

    update

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const jorunalSlice = createSlice({
+    name: 'jorunal',
+    initialState: {  },
+    reducers: {  },
+});
+
+export const {} = jorunalSlice.actions;
