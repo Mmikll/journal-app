1cf218ecbc969d5e35b11ac1481bacff11bfc27d
Miguel <mmirandasanchez16@gmail.com>
2024-10-15 10:32:54 -0300

    changing repo

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const jorunalSlice = createSlice({
+    name: 'jorunal',
+    initialState: { 
+        isSaving: true,
+     },
+    reducers: {  },
+});
+
+export const {} = jorunalSlice.actions;
